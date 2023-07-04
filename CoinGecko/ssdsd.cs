ce1a25276af94f5a2365290b615417ea2a81c9e8
YarikVor <yarikhelov@gmail.com>
2023-07-04 21:00:37 +0300

    delete(moving)

--- CoinGecko/ssdsd.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using CoinGecko.Api.Dto.Coins;
-
-namespace CoinGecko;
-
-public class RootStorage
-{
-    public List<CoinsItemDto> SavedCoins { get; set; } = new();
-}
