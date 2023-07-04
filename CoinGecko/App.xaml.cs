ba9e13a01834f7fcc96a72c6c33630115f51d6ad
YarikVor <yarikhelov@gmail.com>
2023-07-04 21:05:11 +0300

    feat(App): change DI, add conf services and setup configurations

--- CoinGecko/App.xaml.cs
+++ CoinGecko/App.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System.Windows;
-using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using CoinGecho.MemoryApi;
+using CoinGecko.Ex;
+using CoinGecko.LocalStorage;
+using CoinGecko.Managers;
+using CoinGecko.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using YarikVor.QueryFormatting.Services;
@@ -8,49 +15,83 @@ namespace CoinGecko;
 
 public partial class App : Application
 {
+    private const string AppsettingsJson = "appsettings.json";
     private readonly IHost _host;
+    private readonly IServiceProvider _provider;
 
     public App()
     {
+        PreInit();
+
         _host = Host
             .CreateDefaultBuilder()
             .ConfigureServices(ConfigureServices)
+            .ConfigureServices(s => s.AddSingleton<Application>(this))
             .Build();
+
+        _provider = _host.Services;
+    }
+
+    private void PreInit()
+    {
+        try
+        {
+            using var file = File.OpenRead(AppsettingsJson);
+            if (file.Length >= 10) return;
+        }
+        catch
+        {
+            // ignored
+        }
+
+        File.WriteAllText(AppsettingsJson, "{}");
     }
 
     private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
     {
         services
             .AddWindows()
-            //.AddJsonConfiguration()
             .AddPages()
             .AddCoinGeckoClient()
             .AddQueryFormatter()
-            .AddStorageManager();
+            .AddStorageManager()
+            .AddMemoryCache()
+            .AddCoinGeckoService()
+            .AddLocalizationManager()
+            .AddSettingsViewModel()
+            .AddThemeManager();
     }
 
     protected override void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
         _host.Start();
+        MainWindow = _provider.GetRequiredService<MainWindow>();
 
-        var services = _host.Services;
-
-        MainWindow = services.GetRequiredService<MainWindow>();
-
-        var configuration = services.GetRequiredService<IConfiguration>();
+        InitLocalization();
 
         MainWindow.Show();
-
-        base.OnStartup(e);
     }
 
     protected override void OnExit(ExitEventArgs e)
     {
         _host.StopAsync();
-
-
         _host.Dispose();
-
         base.OnExit(e);
+    }
+
+    private void InitLocalization()
+    {
+        var localizationManager = _provider.GetRequiredService<ILocalizationManager>();
+        var themeManager = _provider.GetRequiredService<IThemeManager>();
+        var managerStorage = _provider.GetRequiredService<ManagerStorage>();
+
+        var cultureName = managerStorage.Item.SettingsModel.Culture;
+        var cultureInfo = new CultureInfo(cultureName);
+
+        localizationManager.ChangeLanguage(cultureInfo);
+
+        var themeName = managerStorage.Item.SettingsModel.Theme;
+        themeManager.ChangeTheme(themeName);
     }
 }
